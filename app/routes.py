b9a70cca9ae3aa45fa75866502dcdb86f73994a6
Allen Yin <allen.yin@klook.com>
2019-09-30 15:21:03 +0800

    upload files

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Allen'}
+    return '''
+<html>
+    <head>
+        <title>Home page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
